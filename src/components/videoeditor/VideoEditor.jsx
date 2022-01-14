56063dbb1a35592b01cea95dd96a8099881ef1f5
jasmineblinks <puenehfaith@gmail.com>
2022-01-14 02:03:26 +0100

    fixed

--- src/components/videoeditor/VideoEditor.jsx
+++ src/components/videoeditor/VideoEditor.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import "./editor.css";
-
-import { VideoEditor } from "react-media-editor";
+import { AdvancedVideo } from "@cloudinary/react";
+import { Cloudinary } from "@cloudinary/url-gen";
+import { fill } from "@cloudinary/url-gen/actions/resize";
+import { byRadius } from "@cloudinary/url-gen/actions/roundCorners";
+import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
+import { Gravity } from "@cloudinary/url-gen/qualifiers";
+import { AutoFocus } from "@cloudinary/url-gen/qualifiers/autoFocus";
 
 const VideoEdit = () => {
+  const cld = new Cloudinary({
+    cloud: {
+      cloudName: "demo",
+    },
+  });
+  const myVideo = cld.video("docs/walking_talking");
+
+  // Apply the transformation.
+  myVideo
+    .resize(
+      fill()
+        .width(600)
+        .height(600)
+        .gravity(
+          Gravity.autoGravity().autoFocus(AutoFocus.focusOn(FocusOn.faces()))
+        )
+    ) // Crop the video, focusing on the faces.
+    .roundCorners(byRadius(20)); // R
   return (
     <div className={"header-banner"}>
       <div className={"banner-content-wrapper"}>
@@ -15,7 +38,7 @@ const VideoEdit = () => {
           </p>
         </section>
         <div className={"user-img-wrapper"}>
-          <VideoEditor src="https://your.video.mp4" />
+          <AdvancedVideo cldVid={myVideo} controls />
           {/* <img src={logo} className={"user-img"} /> */}
         </div>
       </div>
