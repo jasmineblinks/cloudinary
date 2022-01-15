575dd055388d39d259fe53664d0926fac825003b
jasmineblinks <puenehfaith@gmail.com>
2022-01-15 06:02:03 +0100

    fixed

--- src/components/videoeditor/VideoEditor.jsx
+++ src/components/videoeditor/VideoEditor.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./editor.css";
 import { AdvancedVideo } from "@cloudinary/react";
 import { Cloudinary } from "@cloudinary/url-gen";
@@ -9,6 +9,10 @@ import { Gravity } from "@cloudinary/url-gen/qualifiers";
 import { AutoFocus } from "@cloudinary/url-gen/qualifiers/autoFocus";
 
 const VideoEdit = () => {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
+
   const cld = new Cloudinary({
     cloud: {
       cloudName: "demo",
@@ -20,13 +24,35 @@ const VideoEdit = () => {
   myVideo
     .resize(
       fill()
-        .width(600)
-        .height(600)
+        .width(150)
+        .height(150)
         .gravity(
           Gravity.autoGravity().autoFocus(AutoFocus.focusOn(FocusOn.faces()))
         )
     ) // Crop the video, focusing on the faces.
     .roundCorners(byRadius(20)); // R
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsSelected(true);
+  };
+  const handleSubmission = () => {
+    const formData = new FormData();
+
+    formData.append("File", selectedFile);
+
+    fetch("https://api.cloudinary.com/v1_1/${cloudName}/upload", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("Success:", result);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
   return (
     <div className={"header-banner"}>
       <div className={"banner-content-wrapper"}>
@@ -38,7 +64,29 @@ const VideoEdit = () => {
           </p>
         </section>
         <div className={"user-img-wrapper"}>
-          <AdvancedVideo cldVid={myVideo} controls />
+          <AdvancedVideo
+            cldVid={myVideo}
+            controls
+            // onPlay={playFunction}
+            // onEnded={endFunction}
+          />
+          <input type="file" name="file" onChange={changeHandler} />
+          {isSelected ? (
+            <div>
+              <p>Filename: {selectedFile.name}</p>
+              <p>Filetype: {selectedFile.type}</p>
+              <p>Size in bytes: {selectedFile.size}</p>
+              <p>
+                lastModifiedDate:{" "}
+                {selectedFile.lastModifiedDate.toLocaleDateString()}
+              </p>
+            </div>
+          ) : (
+            <p>Select a file to show details</p>
+          )}
+          <div>
+            <button onClick={handleSubmission}>Submit</button>
+          </div>
           {/* <img src={logo} className={"user-img"} /> */}
         </div>
       </div>
